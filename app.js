fabbf100bbe0c3e0113e7daaec754f8a176e9cd6
Tyson <tysonjhickey@gmail.com>
2021-06-19 12:29:22 -0230

    Mostly functional. Need to add CSS, and eventually an AI for player to play against.

--- app.js
+++ app.js
@@ -38,6 +38,20 @@ function takeCell(cell) {
         //console.log(currentCell);
         if (currentCell.textContent == "") {
             currentCell.textContent = sessionStorage.getItem("currentPlayer")
+            if (checkWin(currentCell)) {
+                removeOnclick();
+                var br = document.createElement("br");
+                var btn1 = document.createElement("button");
+                btn1.setAttribute("onclick", "reload()");
+                btn1.textContent = "Exit";
+                var btn2 = document.createElement("button");
+                btn2.setAttribute("onclick", "resetBoard()");
+                btn2.textContent = "Reset Board"
+                var container = document.getElementById("boardcontainer");
+                container.appendChild(br);
+                container.appendChild(btn1);
+                container.appendChild(btn2);
+            }
             changePlayer();
             return;
         }
@@ -50,5 +64,67 @@ function changePlayer() {
         sessionStorage.setItem("currentPlayer", "O");
     } else {
         sessionStorage.setItem("currentPlayer", "X");
+    }
+}
+
+function checkWin(cell) {
+    var currentPlayer = sessionStorage.getItem("currentPlayer");
+    var currentCellX = parseInt(cell.id.substring(0,1));
+    var currentCellY = parseInt(cell.id.substring(1,2));
+    var directions = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,1],[1,-1],[-1,-1]]
+    for (var i=0; i < directions.length; i++) {
+        var counter = 1;
+        for (var j=1; j<4; j++) {
+            var modX = directions[i][0] * j;
+            var modY = directions[i][1] * j;
+            if ((currentCellX + modX) >= 0 && (currentCellX + modX) < sessionStorage.getItem("boardSize")
+            && (currentCellY + modY) >= 0 && (currentCellY + modY) < sessionStorage.getItem("boardSize")) {
+                var checkCell = String(currentCellX + modX) + String(currentCellY + modY);
+                checkCell = document.getElementById(checkCell);
+                if (checkCell.innerText == currentPlayer) {
+                    counter++;
+                }
+            }
+        }
+        if (counter == 4) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function reload() {
+    window.location.reload();
+}
+
+function resetBoard() {
+    var table = document.createElement("TABLE");
+    table.setAttribute("id", "board");
+    table.setAttribute("class", "board");
+    for (var i=0; i<sessionStorage.getItem("boardSize"); i++) {
+        var tr = document.createElement("TR");
+        for (var j=0; j<sessionStorage.getItem("boardSize"); j++) {
+            var td = document.createElement("TD");
+            td.setAttribute("onclick", "takeCell(this)");
+            loc = String(j) + String(i);
+            td.setAttribute("id", loc)
+            tr.appendChild(td)
+        }
+        table.appendChild(tr);
+    }
+    var container = document.getElementById("boardcontainer");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    container.appendChild(table)
+    sessionStorage.setItem("currentPlayer", "X");
+}
+
+function removeOnclick() {
+    for (var i=0; i < sessionStorage.getItem("boardSize"); i++) {
+        for (var j=0; j < sessionStorage.getItem("boardSize"); j++) {
+            var currentID = String(i) + String(j);
+            document.getElementById(currentID).removeAttribute("onclick");
+        }
     }
 }
